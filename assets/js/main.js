0272f77c8db8e9f55ba6921f703bbb70a51278ad
NgLeHoang <89005221+NgLeHoang@users.noreply.github.com>
2023-12-11 14:58:55 +0700

    Add Slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,30 @@ $(document).ready(function () {
         $(".nav-link").removeClass("active");
         $(this).addClass("active");
     });
-})
+
+    // Active slide when click next
+    let index = 0;
+    $("#next").on("click", setInterval(function() {
+        $(".car-container").eq(index).removeClass("active");
+        index = (index + 1) % $(".car-container").length;
+        $(".car-container").eq(index).addClass("active");
+    }, 5000));
+
+    // Active slide when click prev
+    $("#prev").on("click", function() {
+        $(".car-container").eq(index).removeClass("active");
+        index = (index - 1 + $(".car-container").length) % $(".car-container").length;
+        $(".car-container").eq(index).addClass("active");
+    })
+
+});
+
+// let index = 0;
+// let slides = document.querySelectorAll('.car-container');
+// function next(){
+//     slides[index].classList.remove('active');
+//     index = (index + 1) % slides.length;
+//     slides[index].classList.add('active');
+// };
+
+
